5136d4d3048d2a9f6ee4f8091c84187c241960e9
FRUSEicFr <FRUSEicFr@99982ebf-a2a1-cd42-bdb3-6dc09f68f89f>
2017-03-15 09:23:00 +0000

    [INT] test
    git-svn-id: https://frsnprsvn1.usr.ingenico.loc/svn/nme-fr/testDirNew@1912 99982ebf-a2a1-cd42-bdb3-6dc09f68f89f

---
+++ essWinsc.bat
@@ -0,0 +1,21 @@
+@echo off
+set path0=%PATH%
+set PATH=%path0%;C:\Program Files\WinSCP
+call :ssh %1 %2>%tmp%\x.txt
+winscp<%tmp%\x.txt
+goto fin
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:ssh
+echo open @%1:22
+echo root
+echo ingenico
+echo cd %2
+echo ls
+echo close
+echo exit
+goto :EOF
+
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:fin
+set PATH=%path0%
